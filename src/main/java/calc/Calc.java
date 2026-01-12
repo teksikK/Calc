bfd75039e34e10a58e629482ce75e100314493e6
teksikK <tex1234121@gmail.com>
2026-01-12 13:43:34 +0100

    first test

---
+++ src/main/java/calc/Calc.java
@@ -0,0 +1,18 @@
+package calc;
+
+public class Calc {
+
+    private int displayValue;
+
+    public Calc() {
+        this.displayValue = 0;
+    }
+
+    public int getDisplayValue() {
+        return displayValue;
+    }
+
+    public void setDisplayValue(int value) {
+        this.displayValue = value;
+    }
+}
